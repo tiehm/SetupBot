c7491c21abf35398af99fadab71a2ceadff77b3f
Charlie <charlie.tiehm@gmail.com>
2018-08-18 21:24:55 +0200

    Updating invite link

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ module.exports = {
     prefix: '-',
     owner: '193394584271847425',
     timeoutBetweenActions: 2000,
-    officialDiscord: 'https://discord.gg/7akGDSy',
+    officialDiscord: 'https://discord.gg/A9cVcjv',
     invite: 'https://discordapp.com/oauth2/authorize/?permissions=1393896696&scope=bot&client_id=480019096709365761'
 };
